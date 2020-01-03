3deba81c8bcae7241c13f68c7e527ffbd3b97f87
MunifTanjim <muniftanjim@gmail.com>
2020-01-03 19:01:54 +0600

    Initial commit

---
+++ src/gatsby-theme-dox/components.js
@@ -0,0 +1,5 @@
+import components from 'gatsby-theme-dox/src/components'
+
+export default {
+  ...components
+}
---
+++ src/gatsby-theme-dox/theme.js
@@ -0,0 +1,12 @@
+import theme from 'gatsby-theme-dox/src/theme'
+
+export default {
+  ...theme,
+  layout: {
+    ...theme.layout,
+    root: {
+      ...theme.layout.root,
+      maxWidth: 1200
+    }
+  }
+}
