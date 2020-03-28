89156c950779620880a89f6f248b450efd1504b4
Munif Tanjim <muniftanjim@gmail.com>
2020-03-28 22:38:07 +0600

    Add some guides

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,10 @@
 module.exports = {
   docs: {
     Synor: ['introduction', 'cli', 'database-engine', 'source-engine'],
-    'Getting Started': ['getting-started/use-as-library'],
+    'Getting Started': [
+      'getting-started/use-as-cli',
+      'getting-started/use-as-library'
+    ],
     Guides: ['guides/create-database-engine', 'guides/create-source-engine']
   }
 }
